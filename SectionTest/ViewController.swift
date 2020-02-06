c33bec5035c95a8a1c4c15e3f2af3ee7b1724587
trungnv <trungnv@ttc-solutions.com>
2020-02-06 15:25:45 +0700

    Initial Commit

---
+++ SectionTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SectionTest
+//
+//  Created by TrungNV on 2/6/20.
+//  Copyright © 2020 trungnv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
